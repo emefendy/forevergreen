07e0bb3bc39fbd122876ae857f974a76aabef38f
Emilie E <108155838+emefendy@users.noreply.github.com>
2024-01-29 09:08:00 -0600

    Add files via upload

---
+++ ForEvergreen/ForEvergreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForEvergreenApp.swift
+//  ForEvergreen
+//
+//  Created by Emilie E. on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ForEvergreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
